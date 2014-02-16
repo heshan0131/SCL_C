344172af5cf1ba035c285e3b58305f587db64289
Shan He <shanhe@mit.edu>
2014-02-16 11:53:54 -0500

    add file

---
+++ js/camera.js
@@ -0,0 +1,38 @@
+
+function camera_move(target_final, distance_final, delta_d){
+	
+	//calculate target steps:
+	var steps = Math.abs((distance_final - distanceTarget) / delta_d);
+	var delta_t = {x: (target_final.x - target.x) / steps, y: (target_final.y - target.y) / steps};
+
+	//distance target
+	function camera_increment(delta_t, delta_d, distance_final) {
+
+		if (delta_d < 0) { // zoom in
+			if (distanceTarget <= distance_final ) {
+				return;
+			} else {		
+				camera_step_in(delta_t, delta_d);
+				saveFrame();
+			}
+		} else {
+			if (distanceTarget >= distance_final ) { // zoom out
+				return;
+			} else {
+				camera_step_in(delta_t, delta_d);
+				saveFrame();
+				
+			}
+		}		
+	}
+
+	function camera_step_in(delta_t, delta_d){
+			distanceTarget += delta_d;
+			target.x += delta_t.x;
+			target.y += delta_t.y;
+			setTimeout(camera_increment, 25, delta_t, delta_d, distance_final);		
+	}
+
+	camera_increment(delta_t, delta_d, distance_final);
+	//rotate
+}
---
+++ js/mouse.js
@@ -0,0 +1,46 @@
+//Mouse events
+
+function onDocumentKeyDown(event) {
+    switch (event.keyCode) {
+      case 38:
+        zoom(2);
+        event.preventDefault();
+        break;
+      case 40:
+        zoom(-2);
+        event.preventDefault();
+        break;
+      case 65:
+        //press A
+        //zoom in to us
+        camera_move(target_1,distanceTarget_1,-3);
+        event.preventDefault();
+        break;
+      case 83:
+        //press S
+        //zoom in to top ones
+        camera_move(target_2,distanceTarget_2,-5);
+        event.preventDefault();
+        break;
+      case 68:
+        //press D
+        //zoom in to ground
+        camera_move(target_3,distanceTarget_3,5);
+        event.preventDefault();
+        break;
+      case 68:
+        //press q
+        //stop
+        cc.stop();
+        event.preventDefault();
+        break; 
+      case 68:
+        //press w
+        //start
+        cc.start();
+        event.preventDefault();
+        break;                   
+
+    }
+  }
+
